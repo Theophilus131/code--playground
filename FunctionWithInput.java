da60e055ee504ff574fac33c90e987a3f0df93db
Theophilus131 <theophilusumar27@gmail.com>
2025-06-09 10:08:05 +0100

    progress

---
+++ FunctionWithInput.java
@@ -0,0 +1,21 @@
+
+	class FunctionWithInput{
+	
+	public static void main(String [] args){
+
+	greet("theophilus");
+	greet("Ada");
+
+
+	}
+	
+	
+	public static void greet(String name){
+
+	System.out.println("Hello, " + name + "");
+	}
+
+
+
+
+	}
