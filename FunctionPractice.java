8f061bd52647f295cadb26568a760cf6e949198b
Theophilus131 <theophilusumar27@gmail.com>
2025-06-09 09:44:07 +0100

    progress

---
+++ FunctionPractice.java
@@ -0,0 +1,39 @@
+public class FunctionPractice{
+
+
+public static void main(String [] args){
+	
+	sayName();
+
+	sayAge();
+
+	sayHobby();
+	
+
+	}
+
+public static void sayName(){
+
+System.out.println("My name is Theophilus: ");
+
+}
+
+public static void sayAge(){
+
+System.out.println("I am 7years old : ");
+
+}
+
+public static void sayHobby(){
+
+System.out.println("I love playing football : ");
+
+}
+
+
+
+
+
+
+
+	}
