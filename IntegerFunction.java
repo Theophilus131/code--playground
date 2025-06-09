b75d72f24c26f47fe374a5fe2f1de82eec650d2d
Theophilus131 <theophilusumar27@gmail.com>
2025-06-09 10:41:29 +0100

    progress

---
+++ IntegerFunction.java
@@ -0,0 +1,18 @@
+
+class IntegerFunction{
+
+public static void main(String [] args){
+
+showAge(76);
+
+}
+
+public static void showAge(int age){
+
+System.out.println("My age is :" + age);
+
+}
+
+
+
+}
---
+++ MultipleParameterFunction.java
@@ -0,0 +1,16 @@
+
+public class MultipleParameterFunction{
+
+public static void main(String [] args){
+
+	greet("Theophils", 7, "football");
+	
+
+	}
+
+public static void greet(String name, int age, String hobby ){
+
+System.out.println("My name is "+ name +" i am "+age+" i love playing "+hobby+" ");
+}
+
+}
